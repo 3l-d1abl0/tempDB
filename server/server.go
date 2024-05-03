15e865b1c1102f6b5c3c76ec0ed018cc802d5433
Sameer <sameer.barha12@gmail.com>
2024-05-04 01:07:01 +0530

    reading commands

--- server/server.go
+++ server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"net"
+	"strings"
 )
 
 type Server struct {
@@ -48,6 +50,45 @@ func (server *Server) Start() {
 		} else {
 			fmt.Println("Not a TCP connection !")
 		}
-	}
 
+		go server.handleConnection(connection)
+	} //for
+
+} //Start
+
+func (server *Server) handleConnection(connection net.Conn) {
+
+	defer connection.Close()
+
+	fmt.Printf("Processing new connection :\n")
+
+	var received int
+
+	buffer := bytes.NewBuffer(nil)
+
+	for {
+
+		chunk := make([]byte, 4096)
+		read, err := connection.Read(chunk)
+		if err != nil {
+			//EOF
+			fmt.Println(received, buffer.Bytes(), err)
+			return
+		}
+
+		received += read
+		buffer.Write(chunk[:read])
+
+		if read == 0 || read < 4096 {
+			fmt.Println(received, buffer.Bytes())
+			//Command
+			fmt.Println((buffer))
+			break
+		}
+	} //for
+
+	cmd := strings.Trim(buffer.String(), "\r\n")
+	args := strings.Split(cmd, " ")
+
+	fmt.Println("[", args, "]")
 }
