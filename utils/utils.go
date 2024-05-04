a1f45d041d703aa2e96e2dbebe1b985a2d94c0d2
Sameer <sameer.barha12@gmail.com>
2024-05-05 02:46:11 +0530

    added commnad parser

---
+++ utils/utils.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"errors"
+	"strings"
+)
+
+type Request struct {
+	Command string
+	Params  []string
+}
+
+func validCommand(cmd string) bool {
+
+	switch cmd {
+
+	case "PING":
+		return true
+	case "GET":
+		return true
+	case "SET":
+		return true
+
+	default:
+		return false
+	}
+}
+
+func ParseCommands(input string) (Request, error) {
+
+	cmd := strings.Trim(input, "\r\n")
+	args := strings.Split(cmd, " ")
+
+	//fmt.Println("[", args, "]")
+
+	//If empty
+	if len(args) == 0 {
+		return Request{}, errors.New("empty command")
+	}
+
+	commandStatus := validCommand(args[0])
+
+	if !commandStatus {
+		return Request{}, errors.New("invalid command")
+	}
+
+	return Request{Command: args[0], Params: args[1:]}, nil
+}
