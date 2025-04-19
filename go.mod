526b6fb4ce5fd8c27aea4a9f98a449139d7569bd
Sameer <sameer.barha12@gmail.com>
2025-04-19 22:37:48 +0530

    updated module dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tempDB
 
-go 1.22.0
+go 1.23
+
+require (
+    github.com/joho/godotenv v1.5.1
+    gopkg.in/yaml.v3 v3.0.1
+)
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "tempDB/server"
 
 func main() {
-
-	server_instance := server.Init(":8090")
-
+	server_instance := server.Init()
 	server_instance.Start()
 }
